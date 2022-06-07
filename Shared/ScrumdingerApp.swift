85b221247ce8e34520b40a03bb842da6924a22e5
Robert Brown <brown.rjb75@gmail.com>
2022-06-07 16:57:51 -0600

    Initial Commit

---
+++ Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by Robert Brown on 2022-06-07.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
